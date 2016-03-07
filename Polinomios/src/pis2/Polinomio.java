e18d9817758ee8d1e5e3117d624a4fc576bfad6d
jorge <jmc766@inlumine.ual.es>
2016-03-07 19:48:22 +0100

    Exporto phyton a java

--- Polinomios/src/pis2/Polinomio.java
+++ Polinomios/src/pis2/Polinomio.java
@@ -6,6 +6,20 @@ public class Polinomio {
 
 	ArrayList<Integer> polinomio = new ArrayList<Integer>();
 	
-	
+	public ArrayList<Integer> suma (ArrayList<Integer> uno, ArrayList<Integer> dos){
+		ArrayList<Integer> sol = new ArrayList<Integer>();
+		//Return the sum of itself and other polynomial
+		//añadir 0 cuando sea de mayor tamaño.
+		if (uno.size() > dos.size()){
+			for (int i=0; i<=uno.size();i++){
+				sol.add(uno.get(i)+dos.get(i));
+			}
+		}
+		else
+			for (int i=0; i<=dos.size();i++){
+				sol.add(dos.get(i)+uno.get(i));
+			}
+		return sol;
+	}
 
 }
