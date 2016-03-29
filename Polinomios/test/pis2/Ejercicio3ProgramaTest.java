c95aa7aa5cb572e23ec3a4f3ad5ea68de1179c11
Esteban <jpc537@inlumine.ual.es>
2016-03-29 13:26:35 +0100

    Empezando el test

---
+++ Polinomios/test/pis2/Ejercicio3ProgramaTest.java
@@ -0,0 +1,14 @@
+package pis2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Ejercicio3ProgramaTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
